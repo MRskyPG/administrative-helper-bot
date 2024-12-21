5128be3f1d6941f5d979f8376886ad059a8b660b
MRskyPG <mrogalsky8113@gmail.com>
2024-12-21 20:32:10 +0700

    Add smiles

---
+++ app/smiles.py
@@ -0,0 +1,13 @@
+# Unicode для смайлов в чате
+
+clock = "\U0001F550"
+pencil = "\U0000270F"
+letter = "\U0001F4E8"
+save_emoji = "\U0001F4BE"
+rocket = "\U0001F680"
+computer = "\U0001F4BB"
+walk_man = "\U0001F6B6"
+
+check_mark = "\U00002705"
+cross_mark = "\U0000274C"
+question_sign = "\U00002753"
